8576bfb70389f99cbee039c94d551c13ee07d536
豆朋伟 <doupengwei@hikvision.com>
2019-08-28 21:19:26 +0800

    first commit

---
+++ src/main/java/com/doudou/myredis/MyRedisApplication.java
@@ -0,0 +1,13 @@
+package com.doudou.myredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRedisApplication.class, args);
+    }
+
+}
